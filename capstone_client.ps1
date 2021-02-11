690c20f1ec343359a7f7cc904da352d57ef50e8a
Liam Kelly <45275395+lmkelly@users.noreply.github.com>
2021-02-11 17:53:56 -0500

    Add files via upload

--- capstone_client.ps1
+++ capstone_client.ps1
@@ -1,10 +1,24 @@
-﻿mkdir C:\.wg | attrib +h C:\.wg
+﻿
+##############
+
+mkdir C:\.wg | attrib +h C:\.wg
 wg genkey | tee C:\.wg\pvt.tmp | wg pubkey > C:\.wg\pub.tmp
 
 $hostname = $env:COMPUTERNAME
 $ip = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
-New-Item -Path ".\$hostname-config.txt" -ItemType "file"
-$pub_key = Get-Content -Path .\pub.tmp
-Add-Content -Path "./$hostname-config.txt" -Value "PublicKey = $pub_key*Endpoint = $ip`:52335"
+New-Item -Path "C:\.wg\$hostname-config.txt" -ItemType "file"
+$pub_key = Get-Content -Path C:\.wg\pub.tmp
+Add-Content -Path "C:\.wg\$hostname-config.txt" -Value "PublicKey = $pub_key*Endpoint = $ip`:52335"
 
-pscp.exe -i C:\Users\arest\Desktop\deployer_key.ppk "$hostname-config.txt" deployer@104.131.19.90:/home/deployer/wg/incoming_config
+$From = "sndcapstone@gmail.com"
+$To = "ostcapstone@gmail.com"
+$Attachment = "C:\.wg\$hostname-config.txt"
+$Subject = "$hostname Config"
+$Body = "sent automatically through the power of my shitty script"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Subject $Subject `
+-Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl `
+-Credential (Get-Credential) -Attachments $Attachment
+
+#pscp.exe -i C:\deployer_key.ppk "C:\.wg\$hostname-config.txt" deployer@104.131.19.90:/home/deployer/wg/incoming_config
